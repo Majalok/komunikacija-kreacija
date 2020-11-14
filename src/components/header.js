eac4712e78822190d4c0ebbc6ff5c0fc8f3742e1
MajaL <maja@reavisys.si>
2020-11-14 18:54:08 +0100

    update

--- src/components/header.js
+++ src/components/header.js
@@ -20,7 +20,7 @@ const Header = ({ siteTitle, subtitle, description }) => (
       {/* <img className="header-logo-img" src="../icons/icon-96x96.png" alt="" />
        */}{" "}
     </div>
-    {/*  <div className="header-main-div">
+    <div className="header-main-div">
       <span className="header-title">
         <h3>
           <Link to="/omeni" className="page-title-link">
@@ -34,12 +34,12 @@ const Header = ({ siteTitle, subtitle, description }) => (
       <div className="header-image-div">
         <img className="header-image" src="../icons/kom-kr-logo-1.png" alt="" />
       </div>
-    </div> */}
-    <div className="header-main-div">
+    </div>
+    {/*  <div className="header-main-div">
       <div className="header-image-div">
         <img className="header-image-test" src="../assets/h1.png" alt="" />
       </div>
-    </div>
+    </div> */}
   </header >
 )
 
