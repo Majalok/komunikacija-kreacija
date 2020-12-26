ebf1da2d3e7b478057232bdaa0fd35647790d32a
MajaL <maja@reavisys.si>
2020-12-26 14:59:27 +0100

    update

--- src/pages/indexObdrziZaPol.js
+++ src/pages/indexObdrziZaPol.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 /* import { graphql } from "gatsby" */
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 /* import Blog from "./blog" */
 /* function initNetlifyIdentity() {
   console.log("initNetlifyIdentity called")
--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 /* import { FaFacebook, FaYoutube, FaMailBulk } from "react-icons/fa" */
 import ContactForm from "../components/ContactForm"
 import ContactCard from "../components/ContactCard"
