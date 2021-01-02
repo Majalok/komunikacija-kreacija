c4ffa8c27ea71911c32203e021c0929061fe17a1
MajaL <maja@reavisys.si>
2021-01-02 18:32:24 +0100

    mail sending... updates

--- src/server/config/prod.js
+++ src/server/config/prod.js
@@ -1,5 +1,5 @@
-module.exports = {  
-    PASSWORD: process.env.PASSWORD, 
-    EMAIL: process.env.EMAIL,  
-    MAIN_URL: process.env.MAIN_URL
+module.exports = {
+  PASSWORD: process.env.PASSWORD,
+  EMAIL: process.env.EMAIL,
+  MAIN_URL: "http://localhost:5001/",
 }
