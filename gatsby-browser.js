340c1d4b15b58beb8759e713f0267a67f48ee3bc
MajaL <maja@reavisys.si>
2021-01-06 00:47:23 +0100

    updatet text

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -29,9 +29,9 @@ export const shouldUpdateScroll = ({
   return false
 }
 
-export const wrapRootElement = ({ element }) => {
+/* export const wrapRootElement = ({ element }) => {
   return (
-   /*  <GoogleReCaptchaProvider
+   <GoogleReCaptchaProvider
       reCaptchaKey="6LfGuxsaAAAAAMbVSYcIXABn5VO183wlMCfNxpwh"
       scriptProps={{
         async: true, // optional, default to false,
@@ -39,6 +39,6 @@ export const wrapRootElement = ({ element }) => {
       }}
     >
       {element}
-    </GoogleReCaptchaProvider> */
+    </GoogleReCaptchaProvider> 
   )
-}
+}*/
