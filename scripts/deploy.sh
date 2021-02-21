c149dd5a180b8b5fe3a3f462de1a0f4be19df585
MajaL <maja@reavisys.si>
2021-02-21 16:27:46 +0100

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var/www/--directory of your middleman site--
+#cd /var/www/--directory of your middleman site--
 
 git fetch
 HEADHASH=$(git rev-parse HEAD)
@@ -12,7 +12,8 @@ then
   echo '****STARTING DEPLOY****'
   echo `date '+%Y-%m-%d %H:%M:%S'`
   git pull 2>&1
-  /usr/local/bin/middleman build 2>&1
+ # /usr/local/bin/middleman build 2>&1
+  gatsby build
   echo '****ENDING DEPLOY****'
   exit 0
 fi
