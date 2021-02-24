3b0bcbb444a4eba0ffe009a74997b7b07f0d9ab4
MajaL1 <majalokar@gmail.com>
2021-02-24 22:28:53 +0100

    Edited bash script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,7 +23,7 @@ then
   cd /home/maja/komunikacija-kreacija
   echo 'Current dir' >> /home/maja/komunikacija-kreacija/scripts/latest.log
   pwd >> /home/maja/komunikacija-kreacija/scripts/latest.log
-  gatsby build
+  .bin/gatsby build
   echo '****ENDING DEPLOY****' >> /home/maja/komunikacija-kreacija/scripts/latest.log
   exit 0
 fi
