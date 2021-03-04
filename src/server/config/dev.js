fee64b76e77b922ee381e82df7e9a86ca7dcccb0
MajaL1 <MajaL1@users.noreply.github.com>
2021-03-04 22:45:03 +0100

    Delete src/server directory

--- src/server/config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  EMAIL: "komunikacija-kreacija@reavisys.si",
-  //EMAIL_TO: "majalokar@gmail.com",
-  EMAIL_TO: "komunikacija.kreacija@gmail.com",
-  //PASSWORD: process.env.PASSWORD
-  // MAIN_URL: "https://komunikacija-kreacija.si/api",
-  MAIN_URL: "https://komunikacija-kreacija.webdev.reavisys.si/api",
-  //MAIN_URL: "http://localhost:5001/",
-}
